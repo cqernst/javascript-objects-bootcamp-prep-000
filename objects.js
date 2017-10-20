1deb95a4a54cd11dd78619e88eb13701639c69d5
Christine QE <christine.q.ernst@gmail.com>
2017-10-20 03:29:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({'Fairuz': 'Shat Iskandaria'});
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
